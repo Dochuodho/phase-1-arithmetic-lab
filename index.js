e7576d47d4af7da48eab579f499667d037f679a4
Ochuodho <ochuodhoderrick@gmail.com>
2023-07-12 14:13:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+const num1 =2;
+const num2 = 31;
+const multiply=num1*num2;
+console.log(multiply);
+
+
+const random=Math.floor(Math.random())+1;
+console.log(random);
+
+const num3=49, num4=5;
+const mod=num3%num4;
+console.log(mod);
+
+const max=Math.max(3,5,8,10,15,20);
+console.log(max);
+
+
+
+
+
